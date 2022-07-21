af0b3137862b6c4dc1d4d04479d429d4630d1c70
Александр Актищев <aktishch@gmail.com>
2022-07-21 12:45:11 +0300

    footer final

--- src/index.js
+++ src/index.js
@@ -49,6 +49,32 @@ function loadHandler() {
 			inputs.forEach((input) => {
 				new maps.SuggestView(input, { results: 5, container: document.body });
 			})
+
+			const map = new maps.Map('map', {
+
+				center: [45.03191007458623,38.921171499999936],
+				zoom: 16
+	
+			})
+	
+			const placemark = new maps.Placemark([45.03191007458623,38.921171499999936], {}, {
+		
+				iconLayout: 'default#image',
+				// iconImageHref: '../img/pictures/geo.png',
+				iconImageSize: [40, 50],
+				iconImageOffset: [-20, -50]
+				
+			})
+	
+			map.controls.remove('geolocationControl')
+			map.controls.remove('searchControl')
+			map.controls.remove('trafficControl')
+			map.controls.remove('typeSelector')
+			map.controls.remove('fullscreenControl')
+			map.controls.remove('zoomControl')
+			map.controls.remove('rulerControl')
+			map.behaviors.disable(['scrollZoom'])
+			map.geoObjects.add(placemark)
 		})
 		.catch(error => console.log('Failed to load Yandex Maps', error));
 }
